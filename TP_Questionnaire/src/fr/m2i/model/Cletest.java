901ff10064d875afd61920c0a84d1b54d7d923de
Administrateur <Administrateur@M2i-34732-2>
2017-02-28 12:24:38 +0100

    12h24

--- TP_Questionnaire/src/fr/m2i/model/Cletest.java
+++ TP_Questionnaire/src/fr/m2i/model/Cletest.java
@@ -1,6 +1,7 @@
 package fr.m2i.model;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 import javax.persistence.Column;
 
@@ -19,17 +20,64 @@ public class Cletest implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(generator="system-uuid")
-	@GenericGenerator(name="system-uuid", strategy="uuid")
-	
 	@Column(name = "cle_unique")
-	@Size(max=6)
-	private String cleunique;
+	
+	private String cleUnique;
+	
+	
+
+	
+	
+	
 
 	@Column(name = "id_test")
-	private int idtest;
+	private int idTest;
 	
 	@Column(name = "id_candidat")
-	private String candidat;
+	private String idCandidat;
+	
+	
+	
+	
+	
+
+
+	public String getCleUnique() {
+		return cleUnique;
+	}
+
+	public void setCleUnique(String cleUnique) {
+		this.cleUnique = cleUnique;
+		
+		
+	}
+
+	public int getIdTest() {
+		return idTest;
+	}
+
+	public void setIdTest(int idTest) {
+		this.idTest = idTest;
+	}
+
+	public String getIdCandidat() {
+		return idCandidat;
+	}
+
+	public void setIdCandidat(String idCandidat) {
+		this.idCandidat = idCandidat;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Cletest() {
+		super();
+		this.cleUnique = UUID.randomUUID().toString().substring(6) ;
+	}
+	
+	
+	
 
 }
