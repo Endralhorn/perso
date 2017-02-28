a6b4f8b16971d3e28dcf969140e7120aff1e9eca
Administrateur <Administrateur@M2i-34732-2>
2017-02-28 15:25:18 +0100

    testDAO

--- TP_Questionnaire/src/fr/m2i/DAO/TestDAO.java
+++ TP_Questionnaire/src/fr/m2i/DAO/TestDAO.java
@@ -1,5 +1,28 @@
+
 package fr.m2i.DAO;
 
-public class TestDAO {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.m2i.model.Test;
+
+@Repository
+@Transactional
+
+public class TestDAO implements IDAO {
+	
+	@PersistenceContext
+	EntityManager em;
+	
+	public void save(Test t){
+		
+		em.persist(t);
+		
+	}
+	
+	
 
 }
