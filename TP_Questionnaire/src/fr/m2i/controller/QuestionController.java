86199c9f7cacf272c8fa27bddba18e37b47c261e
Administrateur <Administrateur@M2i-34732-2>
2017-03-01 11:10:38 +0100

    jhjh

--- TP_Questionnaire/src/fr/m2i/controller/QuestionController.java
+++ TP_Questionnaire/src/fr/m2i/controller/QuestionController.java
@@ -8,24 +8,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.m2i.DAO.AdministrateurDAO;
-import model.Voiture;
+
 
 @Controller
 public class QuestionController {
 	
-	@Autowired
-	AdministrateurDAO adminDAO;
-
-	
-	@RequestMapping("/voitures")
-	public String voitures (Model model ){
-		List<Voiture> listevoiture=this.voitureDAO.findAll();
-		
-		model.addAttribute("listvoiture", listevoiture );
-		
-		
-		
-		return "voitures";
+//	@Autowired
+//	AdministrateurDAO adminDAO;
+//
+//	
+//	@RequestMapping("/voitures")
+//	public String voitures (Model model ){
+//		List<Voiture> listevoiture=this.voitureDAO.findAll();
+//		
+//		model.addAttribute("listvoiture", listevoiture );
+//		
+//		
+//		
+//		return "voitures";
 		
 
 }
