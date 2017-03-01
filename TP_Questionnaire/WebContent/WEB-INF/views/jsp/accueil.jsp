62ba68f33e79becc470c06890cbed47d7897f2a3
Administrateur <Administrateur@M2i-34732-2>
2017-03-01 10:31:47 +0100

    aeaze

--- TP_Questionnaire/WebContent/WEB-INF/views/jsp/accueil.jsp
+++ TP_Questionnaire/WebContent/WEB-INF/views/jsp/accueil.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <%@taglib uri = "http://www.springframework.org/tags" prefix="spring"%>
 
-<h1>Bienvenue ${ utilisateur.prenom } ${ utilisateur.nom } !</h1>
+<h1>Bienvenue ${ administrateur.prenom } ${ administrateur.nom } !</h1>
 
 <form method="post" action="${ pageContext.request.contextPath }/logout">
 <!-- 	<input type="hidden" name="action" value="logout" /> -->
---
+++ TP_Questionnaire/src/fr/m2i/controller/ConnexionController.java
@@ -0,0 +1,30 @@
+package fr.m2i.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import fr.m2i.DAO.AdministrateurDAO;
+import model.Voiture;
+
+@Controller
+public class ConnexionController {
+	
+	@Autowired
+	AdministrateurDAO adminDAO;
+
+	
+	@RequestMapping("/voitures")
+	public String voitures (Model model ){
+		List<Voiture> listevoiture=this.voitureDAO.findAll();
+		
+		model.addAttribute("listvoiture", listevoiture );
+		
+		
+		
+		return "voitures";
+
+}
